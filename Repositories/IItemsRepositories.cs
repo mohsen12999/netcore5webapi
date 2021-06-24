3d291ebe7be057e9b40ed6970d4b9d4c00c2c59c
mohsen <mohsen1299@gmail.com>
2021-06-24 21:51:26 +0430

    add repository interface

---
+++ Repositories/IItemsRepositories.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepositories
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+}
