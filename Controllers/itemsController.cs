0e065e91c8e053e5e12ea9fac62b4621c7aa4ad6
mohsen <mohsen1299@gmail.com>
2021-06-24 23:54:51 +0430

    make update item method

--- Controllers/itemsController.cs
+++ Controllers/itemsController.cs
@@ -58,5 +58,27 @@ namespace Catalog.Controllers
 
             return CreatedAtAction(nameof(GetItems), new { id = item.Id }, item.AsDto());
         }
+
+        // PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdatedItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price
+            };
+
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
     }
 }
