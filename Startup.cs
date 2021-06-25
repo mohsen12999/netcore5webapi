c5942c9d1a6a2d7ba1c508840b957a8ed65446bb
mohsen <mohsen1299@gmail.com>
2021-06-25 12:15:20 +0430

    add mongo setting to startup

--- Startup.cs
+++ Startup.cs
@@ -11,8 +11,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 
 using Catalog.Repositories;
+using Catalog.Setting;
 
 namespace catalog
 {
@@ -28,8 +33,18 @@ namespace catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // for human readable data save data as string in database
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            services.AddSingleton<IMongoClient>(ServiceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
+
             // register repository
-            services.AddSingleton<IItemsRepositories, InMemItemsRepositories>();
+            services.AddSingleton<IItemsRepositories, MongoDbItemsRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
